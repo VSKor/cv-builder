9d20284595eaa8ff5b32ad89d2f18437e25eac04
vskor <vkorolen@qti.qualcomm.com>
2023-04-24 18:12:44 +0300

    feat: added quasar

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import App from './App.vue';
 import router from './router';
 
 import './styles/main.scss';
+import { setupQuasar } from "@/plugins/quasar";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+setupQuasar(app);
 
 app.mount('#app');
---
+++ src/plugins/quasar/index.ts
@@ -0,0 +1,12 @@
+import { Quasar } from 'quasar'
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+import type { App } from "vue";
+
+export const setupQuasar = (app: App) => {
+  return app.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+  });
+};
